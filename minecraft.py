7257084dfd7fac7c836d72879147e20d5acba1a1
David Astudillo <davidadbeelgonzalez@gmail.com>
2022-05-22 10:29:29 -0400

    Agregando los archivos al repo

---
+++ minecraft.py
@@ -0,0 +1,41 @@
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+app = Ursina()
+
+grass_texture = load_texture("assets/grass_block.png")
+stone_texture = load_texture("assets/stone_block.png")
+brick_texture = load_texture("assets/brick_block.png")
+dirt_texture = load_texture("assets/dirt_block.png")
+
+
+class Voxel(Button):
+    def __init__(self, position = (0, 0, 0)):
+        super().__init__(
+            parent = scene,
+            position = position,
+            origin_y = 0.5,
+            color = color.color(0, 0, random.uniform(.9, 1.0)),
+            model = 'cube',
+            texture = 'white_cube',
+            scale = 1
+        )
+
+    def input(self, key):
+        if self.hovered:
+            if key == "left mouse down":
+                voxel = Voxel(position = self.position + mouse.normal)
+            if key == "right mouse down":
+                destroy(self)
+
+
+
+for z in range(20):
+    for x in range(20):
+        voxel = Voxel(position=(x, 0, z))
+
+player = FirstPersonController()
+app.run()
+
+
+#Proyecto terminado
